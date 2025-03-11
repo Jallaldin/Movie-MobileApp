695ca14e1d938710fa70486b653c8b4019bbb75a
Jallaldin Hamdard <jalladinhamdard@gmail.com>
2025-03-11 22:30:43 +0000

    Added TabIcon component for each tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,23 +4,51 @@ import { Tabs } from 'expo-router'
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 
-const TabIcon = () => {
+const TabIcon = ({ focused, icon, title}: any) => {
+    if (focused) {
     return (
         <ImageBackground
             source={images.highlight}
             className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
         >
-            <Image source={icons.home}
+            <Image source={icon}
                    tintColor="#151312" className="size-5" />
-            <Text className="text-secondary text-base font-semihold ml-2">Home
+            <Text className="text-secondary text-base font-semihold ml-2">{title}
             </Text>
         </ImageBackground>
+    )}
+
+    return (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+            <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+        </View>
     )
 }
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '0f0d23'
+
+            }
+        }}>
         <Tabs.Screen
             name="index"
             options={{
