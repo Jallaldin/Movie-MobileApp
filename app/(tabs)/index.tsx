c5b89ae2708c5e19593e58c3d3189750ac993993
Jallaldin Hamdard <jalladinhamdard@gmail.com>
2025-03-12 22:50:01 +0000

    Design bg and add icon for Home,Import SearchBar component, on press send to search page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,55 @@
-import { Text, View } from "react-native";
+import {ActivityIndicator, Image, ScrollView, Text, View} from "react-native";
 import { Link } from 'expo-router';
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
+import SearchBar from "@/components/searchBar";
+import { useRouter } from 'expo-router';
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
 
 export default function Index() {
+    const router = useRouter();
+
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError
+    } = useFetch(() => fetchMovies({
+        query: ''
+    }))
+
   return (
-    <View className="flex-1 justify-center items-center">
-        <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+    <View className='flex-1 bg-primary'>
+        <Image source={images.bg} className='absolute w-full z-0'/>
+
+        <ScrollView className='flex-1 px-5' showsVerticalScrollIndicator={false} contentContainerStyle={{
+            minHeight: '100%', paddingBottom: 10
+        }}>
+            <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto'/>
+
+            {moviesLoading ? (
+                <ActivityIndicator
+                    size='large'
+                    color='white'
+                    className='mt-10 self-center'
+                />
+            ) : moviesError ? (
+                <Text className='text-white'>Error: {moviesError?.message}</Text>
+            ) : (
+                <View className='flex-1 mt-5'>
+                    <SearchBar
+                        onPress={() => router.push('/search')}
+                        placeholder='Search for a movie'
+                    />
+
+                    <>
+                        <Text className='text-lg text-white fold-bold mt-5 mb-3'>Latest Movies</Text>
+                    </>
+                </View>
+            )}
+
+
+        </ScrollView>
     </View>
   );
 }
